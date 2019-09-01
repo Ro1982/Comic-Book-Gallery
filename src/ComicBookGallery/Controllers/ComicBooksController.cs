69a67609bba402404f6da97bd16946a18ab49c74
Rachelle Anderson <Rachelleande@gmail.com>
2019-09-01 19:16:43 -0400

    Updated Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,6 +12,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spiderman";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor's Octopus' life and his one, last, great act of revenge! Even if Spider-man survives...will Peter Parker?";
+            ViewBag.Artists = new string[]
+            {
+                "script: dan slott",
+                "pencils: humberto ramos",
+                "inks: victor olazaba",
+                "colors: edgar delgado",
+                "letters: chris eliopoulos"
+            };
+
             return View();
 
         }
